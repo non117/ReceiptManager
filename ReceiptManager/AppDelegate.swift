f462f91e6eb8b5cf90f2ab4a58581bfbd60467a0
non <mail@non117.com>
2016-10-10 21:17:50 +0900

    Initial Commit

---
+++ ReceiptManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ReceiptManager
+//
+//  Created by non on 2016/10/10.
+//  Copyright © 2016年 non. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ReceiptManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ReceiptManager
+//
+//  Created by non on 2016/10/10.
+//  Copyright © 2016年 non. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
