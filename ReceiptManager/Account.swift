7088fed10b00079021690cb7c7dc82b3d4d29112
non <mail@non117.com>
2017-04-04 22:32:20 +0900

    商品とお店のクラスを追加

---
+++ ReceiptManager/Account.swift
@@ -0,0 +1,15 @@
+//
+//  Account.swift
+//  ReceiptManager
+//
+//  Created by non on 2017/04/03.
+//  Copyright © 2017年 non. All rights reserved.
+//
+
+import Foundation
+
+public struct Account {
+    let id: Int
+    let name: String
+    let parentId: Int
+}
---
+++ ReceiptManager/Product.swift
@@ -0,0 +1,15 @@
+//
+//  Product.swift
+//  ReceiptManager
+//
+//  Created by non on 2017/04/03.
+//  Copyright © 2017年 non. All rights reserved.
+//
+
+import Foundation
+
+public struct Product {
+    let price: Int
+    let name: String
+    let account: Account
+}
