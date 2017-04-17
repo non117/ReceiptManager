62377202c6e922bdce2ce076ab7d7fe83a01f6b0
non <mail@non117.com>
2017-04-17 22:23:03 +0900

    jsonをテストデータ使うように

--- ReceiptManager/ViewController.swift
+++ ReceiptManager/ViewController.swift
@@ -11,26 +11,41 @@ import Cocoa
 class ViewController: NSViewController {
 
     @IBOutlet weak var imageView: NSImageView!
-    @IBOutlet var receiptController: NSArrayController!
-    var receipt: Receipt!
+    @IBOutlet var receiptController: NSObjectController!
+    var receiptForm: ReceiptForm!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let imagePath = URL(fileURLWithPath: "/Users/non/Desktop/IMG_0001.JPG")
-        self.imageView.image = NSImage(contentsOf: imagePath)
-        
-        let client = OCRClient(apiKey: "")
-        client.annotate(imagePath: imagePath, responseHandler: {(res: AnnotatedResponse?) in
-            self.receipt = res?.toReceipt()
-            self.receiptController.content = self.receipt.texts
-        })
+        self.testDataLoad()
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+    
+    private func testDataLoad() {
+        let imagePath = URL(fileURLWithPath: "/Users/non/Desktop/IMG_0001.JPG")
+        self.imageView.image = NSImage(contentsOf: imagePath)
+        
+        //let client = OCRClient(apiKey: "")
+        //client.annotate(imagePath: imagePath, responseHandler: {(res: AnnotatedResponse?) in
+        //    let receipt = Receipt(response: res!)
+        //    self.receiptForm = ReceiptForm(receipt: receipt)
+        //    self.receiptController.content = self.receiptForm
+        //})
+        let testJson = URL(fileURLWithPath: "/Users/non/Desktop/working/res.json")
+        guard let res = try? Data(contentsOf: testJson) else {
+            return
+        }
+        let jsonData = try? JSONSerialization.jsonObject(with: res)
+        guard let annotatedResponse = try? AnnotatedResponse.decodeValue(jsonData) else {
+            return
+        }
+        let receipt = Receipt(response: annotatedResponse)
+        self.receiptForm = ReceiptForm(receipt: receipt)
+        self.receiptController.content = self.receiptForm
     }
 }
 
