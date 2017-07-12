e588f7fe887fa86a3f93552cd35b6eea8c686d45
non <mail@non117.com>
2017-07-12 22:35:06 +0900

    json export用のクラスを追加

---
+++ ReceiptManager/PaymentRecord.swift
@@ -0,0 +1,25 @@
+//
+//  PaymentRecord.swift
+//  ReceiptManager
+//
+//  Created by non on 2017/07/12.
+//  Copyright © 2017年 non. All rights reserved.
+//
+
+import Foundation
+
+public struct PaymentRecord {
+    let date: Date
+    let price: Int
+    let name: String
+    let account: Account
+    
+    func toJSON() -> [String: Any] {
+        var dictionary: [String: Any] = [:]
+        dictionary["date"] = date
+        dictionary["price"] = price
+        dictionary["name"] = name
+        dictionary["account"] = account
+        return dictionary
+    }
+}
